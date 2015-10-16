1103a76ceeb11912610b16af7b7162a6be096bd8
Daniel Banck <daniel@dbanck.de>
2015-10-17 01:51:03 +0200

    Removed gulp-imagemin due to strange travis errors

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -49,10 +49,6 @@ gulp.task('jshint', () =>
 // Optimize images
 gulp.task('images', () =>
   gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
     .pipe(gulp.dest('dist/images'))
     .pipe($.size({title: 'images'}))
 );
